acdd0ff36c65b00e9ceaad40a90a23a697136b7f
周良聪 <13414855775@163.com>
2021-04-06 16:20:59 +0800

    修复登陆

--- acs/src/main/java/com/sw/acs/shiro/matcher/PasswordMatcher.java
+++ acs/src/main/java/com/sw/acs/shiro/matcher/PasswordMatcher.java
@@ -1,8 +1,11 @@
 package com.sw.acs.shiro.matcher;
 
+import com.sw.acs.utils.AcsSecurityUtils;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.credential.CredentialsMatcher;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,7 +15,10 @@ import org.springframework.stereotype.Component;
 public class PasswordMatcher implements CredentialsMatcher {
     @Override
     public boolean doCredentialsMatch(AuthenticationToken authenticationToken, AuthenticationInfo authenticationInfo) {
+        SimpleAuthenticationInfo info = (SimpleAuthenticationInfo)authenticationInfo;
+        String salt = new String(info.getCredentialsSalt().getBytes());
+        String encryptPassword = AcsSecurityUtils.encryptPassword((String)authenticationToken.getCredentials(),salt);
         return authenticationToken.getPrincipal().toString().equals(authenticationInfo.getPrincipals().toString())
-                && authenticationToken.getCredentials().toString().equals(authenticationInfo.getCredentials().toString());
+                && encryptPassword.equals(authenticationInfo.getCredentials().toString());
     }
 }
